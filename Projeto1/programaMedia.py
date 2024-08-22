8e09ac5331666b51a5cc8254410acc559dc2a92d
Dedel <idelvitor71@gmail.com>
2024-08-22 15:20:58 -0300

    Segundo commit

--- Projeto1/programaMedia.py
+++ Projeto1/programaMedia.py
@@ -5,7 +5,16 @@ numero2 = int(input('Me diga um numero: '))
 
 print(fn.media(numero1, numero2))
 
+while True:
+    sair = input('Se desejar sair, digite "s": ').strip().lower()
 
-
-
+    if len(sair) > 1:
+        print('Dado inválido, digite novamente!')
+    else: 
+        if sair == 's':
+            print('Obrigado por utilizar o app')
+            break
+        else:
+            print('Você optou por continuar.')
+            break
 
