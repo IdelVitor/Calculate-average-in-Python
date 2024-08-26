400a02547337ec788d7a74f6497a9da24c5c2270
IdelVitor <115672811+IdelVitor@users.noreply.github.com>
2024-08-26 12:38:44 -0300

    Delete Estudando directory

--- Estudando/Calculadora de IMC.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual o seu nome: ')
-altura = float(input('Qual a sua altura: '))
-peso = float(input('Qual é seu peso: '))
-imc = peso / (altura ** 2)
-print(f'{nome}, seu IMC é = {imc:.2f}')
