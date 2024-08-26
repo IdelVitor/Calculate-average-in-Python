fd8c1c481019c6ea485294c3fc656fd098ae0dc1
IdelVitor <115672811+IdelVitor@users.noreply.github.com>
2024-08-26 12:38:52 -0300

    Delete Projeto2 directory

--- Projeto2/funçãoParImpar.py
+++
@@ -1,6 +0,0 @@
-def parimpar(numero):
-
-    if numero % 2 == 0:
-        print('O numero escolhido é par')
-    else:
-        print('O numero escolhido é impar')
--- Projeto2/progamaParImpar.py
+++
@@ -1,4 +0,0 @@
-import funçãoParImpar as fn
-
-numero = int(input('Me diga um numero: '))
-print(fn.parimpar(numero))
